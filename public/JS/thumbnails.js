f410468c3c6f9603e4dc75c0e63d74c10aa6bd22
Wil Coder <terraxgamer1@gmail.com>
2024-08-06 14:17:09 -0500

    2.2

--- public/JS/thumbnails.js
+++ public/JS/thumbnails.js
@@ -1,59 +1,59 @@
 const THUMBNAILS = [
   {
     alt: "Thumbnail 0",
-    src: "/public/img/0.webp"
+    src: "/img/0.webp"
   },
   {
     alt: "Thumbnail 1",
-    src: "/public/img/1.webp"
+    src: "/img/1.webp"
   },
   {
     alt: "Thumbnail 2",
-    src: "/public/img/2.webp"
+    src: "/img/2.webp"
   },
   {
     alt: "Thumbnail 3",
-    src: "/public/img/3.webp"
+    src: "/img/3.webp"
   },
   {
     alt: "Thumbnail 4",
-    src: "/public/img/4.webp"
+    src: "/img/4.webp"
   },
   {
     alt: "Thumbnail 5",
-    src: "/public/img/5.webp"
+    src: "/img/5.webp"
   },
   {
     alt: "Thumbnail 6",
-    src: "/public/img/6.webp"
+    src: "/img/6.webp"
   },
   {
     alt: "Thumbnail 7",
-    src: "/public/img/7.webp"
+    src: "/img/7.webp"
   },
   {
     alt: "Thumbnail 8",
-    src: "/public/img/8.webp"
+    src: "/img/8.webp"
   },
   {
     alt: "Thumbnail 9",
-    src: "/public/img/9.webp"
+    src: "/img/9.webp"
   },
   {
     alt: "Thumbnail 10",
-    src: "/public/img/10.webp"
+    src: "/img/10.webp"
   },
   {
     alt: "Thumbnail 11",
-    src: "/public/img/11.webp"
+    src: "/img/11.webp"
   },
   {
     alt: "Thumbnail 12",
-    src: "/public/img/12.webp"
+    src: "/img/12.webp"
   },
   {
     alt: "Thumbnail 13",
-    src: "/public/img/13.webp"
+    src: "/img/13.webp"
   }
 ];
 export default THUMBNAILS;
